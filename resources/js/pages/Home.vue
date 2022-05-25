11c12940427ab93e7613591a71190dec208fe12b
Jim Katsioulas <katsioulasdim@gmail.com>
2022-05-25 22:48:49 +0300

    Initial commit

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h4>Homepage</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    data() {
+        return {
+            //
+        }
+    },
+    created() {
+    },
+    methods: {}
+}
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello from Shouts.dev!</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="text-center" style="margin: 50px 0 10px 0;">
+            Hello from Shouts.dev!
+        </div>
+    </div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
