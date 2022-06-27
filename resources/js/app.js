b2dc13bedb17506661541b389cae4dd89e4bb33e
Jim Katsioulas <katsioulasdim@gmail.com>
2022-06-27 20:42:01 +0300

    Some other initials, nav-bar, side-bar and footer

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,17 @@
 import {createApp} from 'vue'
 
+
+
 require('./bootstrap')
 import App from './App.vue'
 import axios from 'axios'
 import router from './router'
+import { mapStateTwoWay } from 'vuex-map-state';
 
 const app = createApp(App)
 app.config.globalProperties.$axios = axios;
 app.use(router)
+
+app.use({mapStateTwoWay})
+
 app.mount('#app')
